645ee75698d8d03f2b79887e324833ea03c2e18d
beyondbrokkoli <halim.zag@gmx.de>
2026-09-05 20:08:59 +0200

    static_single_assignment

--- tests/phases/pA.lua
+++
@@ -1,7 +0,0 @@
-local pa_t = {}
-local pa_n = 400
-local pa_i = 0
-while pa_i < pa_n do
-    pa_t[pa_i] = pa_i + 1
-    pa_i = pa_i + 2
-end
--- tests/phases/pI.lua
+++
@@ -1,8 +0,0 @@
-local pi_t = {}
-local pi_flag = 0 < 1
-local pi_k = 0
-while pi_flag do
-    pi_t[pi_k] = 100
-    pi_flag = 0 < 0
-    pi_k = pi_k + 1
-end
